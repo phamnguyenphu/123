fee6c9011e1af9b16c8d048594f103911e63efba
phamnguyenphu <phamnguyenphucp9@gmail.com>
2020-11-10 11:59:52 +0700

    first commit

---
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        
+    }
+}
